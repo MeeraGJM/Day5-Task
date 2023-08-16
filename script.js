1767742f10ea54e4e13c59afce3b91b9b2eb7b69
MeeraGJM <136172957+MeeraGJM@users.noreply.github.com>
2023-08-16 13:04:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let myResume={
+    "biodata":
+    {
+        
+        "name":"MEERA J",
+        "email":"connectmeera19@gmail.com",
+        "phone":8489477398,
+        "degree":"M.C.A",
+        "adress":
+            {
+            "no": "168/2, Big Street, Chinnavippedu Village",
+            "pincode":603109,
+            "city":"chennai",
+            "state":"Tamilnadu",
+            "country":"India"
+        }
+    }, 
+        "education":
+            {
+            "degree":"M.C.A",
+            "collegeName": "B.S.Abdur Rehman Crescent Institute of Science and Technology",
+            "department": "Computer Secience",
+            "gpa":7.7
+        },
+    
+    
+        "skils":" basic of java, html, css, javascript",
+        "languages":"Tam,il, English",
+    
+    
+}
+console.log(myResume);
+    
